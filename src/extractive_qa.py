dd1fcd8abefd2fb798633bbe64eb83b683fd9b4e
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-10-18 11:45:36 +0530

    fix datatype issue

--- src/extractive_qa.py
+++ src/extractive_qa.py
@@ -6,9 +6,18 @@ from dotenv import load_dotenv
 
 class ExtractiveQA:
     def __init__(self, claim_data: str):
-        self.claim_data = claim_data if isinstance(claim_data, str) else str(claim_data)
+        self.claim_data = claim_data if isinstance(
+            claim_data, str) else str(claim_data)
+        if isinstance(self.claim_data, dict):
+            self.claim_data = self._process_data()
         self.model_name = read_config('EXTRACTIVE_QA_MODEL_NAME')
-        print(self.model_name)
+
+    def _process_data(self):
+        details_str = "\n".join(
+            f"{key}: {value}" for key, value in self.claim_data.items() if key != "Clinical_note")
+        context = f"{details_str}\nClinical Note:\n{self.claim_data.get('Clinical_note', '')}"
+        return context
+
     def qa(self, question: str):
         try:
             if not self.claim_data or not question:
@@ -18,7 +27,6 @@ class ExtractiveQA:
                 provider="hf-inference",
                 api_key=os.getenv('HF_TOKEN'),
             )
-            print(os.getenv('HF_TOKEN'))
             answer = client.question_answering(
                 question=question,
                 context=self.claim_data,
@@ -34,8 +42,5 @@ if __name__ == "__main__":
     from src.data_ingestion import DataIngestion
     data_ingestion = DataIngestion(patient_id="PA-12345")
     claim_data = data_ingestion.ingest_claim_data(claim_path='CLM153910000')
-    print(type(claim_data))
-    context = claim_data if isinstance(claim_data, str) else str(claim_data)
-    print(type(context))
-    extractive_qa = ExtractiveQA(claim_data=context)
+    extractive_qa = ExtractiveQA(claim_data=claim_data)
     print(extractive_qa.qa("Who is the doctor?"))
