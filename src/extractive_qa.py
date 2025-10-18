1e34ffd27b4494625e7f412e3ddb2b84494afdf0
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-10-18 11:40:29 +0530

    extractive qa

---
+++ src/extractive_qa.py
@@ -0,0 +1,41 @@
+from src.config import read_config
+from huggingface_hub import InferenceClient
+import os
+from dotenv import load_dotenv
+
+
+class ExtractiveQA:
+    def __init__(self, claim_data: str):
+        self.claim_data = claim_data if isinstance(claim_data, str) else str(claim_data)
+        self.model_name = read_config('EXTRACTIVE_QA_MODEL_NAME')
+        print(self.model_name)
+    def qa(self, question: str):
+        try:
+            if not self.claim_data or not question:
+                return None
+            load_dotenv()
+            client = InferenceClient(
+                provider="hf-inference",
+                api_key=os.getenv('HF_TOKEN'),
+            )
+            print(os.getenv('HF_TOKEN'))
+            answer = client.question_answering(
+                question=question,
+                context=self.claim_data,
+                model=self.model_name,
+            )
+            return answer
+        except Exception as e:
+            print(f"Exception: {e}")
+            return None
+
+
+if __name__ == "__main__":
+    from src.data_ingestion import DataIngestion
+    data_ingestion = DataIngestion(patient_id="PA-12345")
+    claim_data = data_ingestion.ingest_claim_data(claim_path='CLM153910000')
+    print(type(claim_data))
+    context = claim_data if isinstance(claim_data, str) else str(claim_data)
+    print(type(context))
+    extractive_qa = ExtractiveQA(claim_data=context)
+    print(extractive_qa.qa("Who is the doctor?"))
